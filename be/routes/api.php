b5d9de9c1e7b729a32fd382fca4a9208d183254b
Irul Huda <irulhuda983@gmail.com>
2023-07-30 19:08:14 +0700

    add storage

--- be/routes/api.php
+++ be/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\JenisBarangController;
